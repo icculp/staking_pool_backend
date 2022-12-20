36f36112208ba4ab2dd6f2d0bfe31ba58fdc11fd
Ian Culp <icculp@gmail.com>
2022-12-20 23:01:30 +0000

    env.PORT

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useLogger(new MyLogger())
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
